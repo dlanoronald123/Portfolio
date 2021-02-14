2d03543283f2d251fadf1257b2d3f8f067311519
Ronald Magno <74632509+dlanoronald123@users.noreply.github.com>
2021-02-14 14:48:46 +0800

    Add files via upload

---
+++ ProductLandingPage/app.js
@@ -0,0 +1,69 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+  body.classList.add(theme);
+
+}
+
+// Button Event Handlers
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
+};
+
+
+
+
+
+
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
+
