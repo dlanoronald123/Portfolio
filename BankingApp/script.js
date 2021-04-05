06e118f5ba645101fe07eb650f5e4b3a35ebff0d
Ronald Magno <74632509+dlanoronald123@users.noreply.github.com>
2021-04-05 12:02:22 +0800

    Add files via upload

---
+++ BankingApp/script.js
@@ -0,0 +1,113 @@
+let create = document.querySelector('.createAccount');
+let createForm = document.querySelector('.form');
+let createBtn = document.querySelector('#createBtn');
+let inputs = document.querySelectorAll('.text');
+let tbody = document.querySelector('#tbody');
+let users = JSON.parse(localStorage.getItem('users'));
+
+if (users === null){
+    users = [];
+}
+
+loadTable();
+
+function list_users(){
+    return users;
+}
+
+function loadTable(){
+    if (tbody !== null){
+        tbody.innerHTML = "";
+        if (parseInt(list_users().length) === 0) tbody.innerHTML = "</tr><td class = 'emptyData' colspan ='4'>Empty Data</td></tr>";
+        for (let i = 0; i < list_users().length; i++){
+            let tr = "<tr class='tableRow'>";
+            let fullName = list_users()[i].name;
+            let amount = list_users()[i].amount;
+            tr += "<td class='entry1'>" + (i + 1) + "</td>" + "<td class='entry2'>" + fullName + "</td>" + "<td class='entry3'>" + get_balance(amount) + "</td><td class='entry4'><div class='deleteBtn'><i class= 'fa fa-backspace'></i></div></td></tr>";
+            tbody.innerHTML += tr;
+        }
+    }
+}
+
+function get_balance(user){
+    let value = Number(parseFloat(user).toFixed(2)).toLocaleString('en',{
+        minimumFractionDigits: 2
+    });
+    return `₱${value}`;
+}
+
+function create_user(user, balance) {
+    let accountObj = {
+        name: user,
+        amount: parseFloat(balance)
+    }
+    if (balance === null || balance === undefined || balance === ''){
+        accountObj.amount = 0; 
+    }
+    let userStatus = true;
+    for (const fullname of users) {
+        if (fullname.name.toLowerCase() === user.toLowerCase()) userStatus = false;
+    }
+    if (userStatus) {
+        users.push(accountObj);
+        localStorage.setItem('users', JSON.stringify(users));
+        alert(`Account named ${user} had been successfully created.`);
+    } else {
+        alert(`Name ${user} already exist.`);
+    }
+}
+
+function createSubmit() {
+    if (createForm) {
+        var elements = createForm.elements;
+        var user = {};
+        for (let i = 0; i<elements.length; i++) {
+            var item = elements.item(i);
+            user[item.name] = item.value.trim();
+        }
+        user.fullname = function () {
+            return this.firstname + ' ' + this.lastname;
+        }
+        if (user.fullname()){
+            if (parseFloat(user.amount) > 0){
+                create_user(user.fullname(), user.amount);
+            }
+            else {
+                alert('Invalid amount.');
+            }
+        }
+        else {
+            alert('Must contain letters only.');
+        }
+        for (let i = 0; i<elements.length; i++){
+            var item = elements.item(i);
+            item.value = '';
+        }
+        loadTable();
+    }
+    return false;
+}   
+    if (tbody.children){
+        let deleteButton = document.querySelectorAll('.deleteBtn');
+        for (const deleteBtn of deleteButton){
+            deleteBtn.addEventListener('click', function(){
+                let user = deleteBtn.parentNode.parentNode.children[1].innerHTML;
+                let confirmDelete = confirm(`Do you want to delete the account of ${user}?`);
+                if (confirmDelete){
+                    deleteAccount(user);
+                    alert ('Account deleted.')
+                    loadTable();
+                }
+            });
+        }
+    }
+
+function deleteAccount(user){
+    for (const [i, fullname] of users.entries()) {
+        if (fullname.name.toLowerCase() === user.toLowerCase()) {
+            users.splice(i,1);
+        }
+    }
+    localStorage.setItem('users',JSON.stringify(users));
+    location.reload();
+}
---
+++ BankingApp/transact.js
@@ -0,0 +1,197 @@
+let depositBtn = document.querySelector('.depositBtn');
+let withdrawBtn = document.querySelector('.withdrawBtn');
+let transferBtn = document.querySelector('.transferBtn');
+
+let depositForm = document.querySelector('.depositForm');
+let withdrawForm = document.querySelector('.withdrawForm');
+let transferForm = document.querySelector('.transferForm');
+
+let tbody = document.querySelector('#tbody');
+let inputs = document.querySelectorAll('.text');
+let users = JSON.parse(localStorage.getItem('users'));
+
+let state = true;
+
+if (users === null) {
+    users = [];
+}
+
+loadTable();
+
+function list_users() {
+    return users;
+}
+
+function loadTable() {
+    if (tbody !== null) {
+        tbody.innerHTML = "";
+        if (parseInt(list_users().length) === 0) tbody.innerHTML = "</tr><td class='noData'colspan='4'>No Data</td></tr>";
+        for (let i = 0; i < list_users().length; i++) {
+            let tr = "<tr class='dashboardRow'>";
+            let fullName = list_users()[i].name;
+            let amount = list_users()[i].amount;
+            tr += "<td class='col1'>" + (i + 1) + "</td>" + "<td class='col2'>" + fullName + "</td>" + "<td class='col3'>" + get_balance(amount) + "</td></tr>";
+            tbody.innerHTML += tr;
+        }
+    }
+}
+
+function get_balance(user) {
+    let value = Number(parseFloat(user).toFixed(2)).toLocaleString('en', {
+        minimumFractionDigits: 2
+    });
+    return `₱${value}`;
+}
+
+function deposit(user, amount) {
+    let state = false;
+    if (parseFloat(amount) > 0) {
+        for (const account of users) {
+            if (account.name.toLowerCase() === user.toLowerCase()) {
+                account.amount = parseFloat(amount) + parseFloat(account.amount);
+                localStorage.setItem('users', JSON.stringify(users));
+                alert('Deposited ' + get_balance(amount)+ ' to ' + user +"'s Account.");
+                state = true;
+            }
+        } 
+        if (!state) alert('Please input a valid account.');
+    } 
+}
+
+function withdraw(user, amount) {
+    let state = false;
+    let insufficient = false;
+    if (parseFloat(amount) > 0) {
+        for (const account of users) {
+            if (account.name.toLowerCase() === user.toLowerCase()) {
+                state = true;
+                if (parseFloat(account.amount) < parseFloat(amount)) {
+                    insufficient = true;
+                } else {
+                    account.amount = parseFloat(account.amount) - parseFloat(amount);
+                    localStorage.setItem('users', JSON.stringify(users));
+                    alert('Withdrawn '+ get_balance(amount) + ' from ' + user +"'s Account.");
+                }
+            }
+        }
+        if (!state) alert('Please input a valid account.');
+        if (insufficient) alert(user + ' has insufficient balance to withdraw the requested amount.');
+    } 
+}
+
+function send(from_user, to_user, amount) {
+    let insufficient = false;
+    let fromState = false;
+    let toState = false;
+    let alert1 = '';
+    let alert2 = '';
+    for (const check of users) {
+        if (check.name.toLowerCase() === from_user.toLowerCase()) fromState = true;
+        if (check.name.toLowerCase() === to_user.toLowerCase()) toState = true;
+    }
+    if (fromState && toState) {
+        for (const account of users) {
+            if (parseFloat(amount) > 0) {
+                if (account.name.toLowerCase() === from_user.toLowerCase()) {
+                    state = true;
+                    if (parseFloat(account.amount) < parseFloat(amount)) {
+                        insufficient = true;
+                    } else {
+                        account.amount = parseFloat(account.amount) - parseFloat(amount);
+                    }
+                }
+                if (account.name.toLowerCase() === to_user.toLowerCase() && !insufficient) {
+                    account.amount = parseFloat(amount) + parseFloat(account.amount);
+                }
+            }
+        }
+        if (insufficient) {
+            alert(from_user + ' has insufficient balance to transfer the requested amount.');
+        }else{
+                alert(`Transferred ${get_balance(amount)} from ${from_user} to ${to_user}'s Account.`); 
+             }      
+        }else{
+            if (!fromState) alert1 = 'Please put valid Sender';
+            if (!toState) alert2 = 'Please put valid Receiver';
+            alert(alert1 + '\n' + alert2);
+             }
+        localStorage.setItem('users', JSON.stringify(users));
+    }
+
+
+function stringFilter(str) {
+    let arrayString = str.split('');
+    let state = true;
+    for (const [i, findspace] of arrayString.entries()) {
+        if (findspace === " ") arrayString.splice(i, 1);
+    }
+    for (const letter of arrayString) {
+        if (!isNaN(letter)) { 
+            state = false; 
+        }
+    }
+    return state;
+}
+
+
+depositBtn.addEventListener('click', function () {
+        let elements = depositForm.elements;
+        let user = {};
+        for (let i = 0; i < elements.length; i++) {
+            let item = elements.item(i);
+            user[item.name] = item.value.trim();
+        }
+        user.fullname = function () {
+            return this.firstname + ' ' + this.lastname;
+        }
+        if (stringFilter(user.fullname())) {
+            deposit(user.fullname(), user.amount);   
+        }
+        else {
+            alert('Must contain letters only.')
+        }
+});
+
+withdrawBtn.addEventListener('click', function () {
+        let elements = withdrawForm.elements;
+        let user = {};
+        for (let i = 0; i < elements.length; i++) {
+            let item = elements.item(i);
+            user[item.name] = item.value.trim();
+        }
+        user.fullname = function () {
+            return this.firstname + ' ' + this.lastname;
+        }
+        if (stringFilter(user.fullname())) {
+            withdraw(user.fullname(), user.amount);
+        }
+        else {
+            alert('Must contain letters only.')
+        }
+});
+
+transferBtn.addEventListener('click', function () {
+        let elements = transferForm.elements;
+        let user = {};
+        for (let i = 0; i < elements.length; i++) {
+            let item = elements.item(i);
+            user[item.name] = item.value.trim();
+        }
+        user.senderFullname = function () {
+            return this.senderFirstname + ' ' + this.senderLastname;
+        }
+        user.receiverFullname = function () {
+            return this.receiverFirstname + ' ' + this.receiverLastname;
+        }
+        if (stringFilter(user.senderFullname()) && stringFilter(user.receiverFullname())) {
+            if (user.senderFullname().toLowerCase() === user.receiverFullname().toLowerCase()) {
+                alert('The account were the same.');
+            }
+            else {
+                send(user.senderFullname(), user.receiverFullname(), user.senderAmount);
+            }
+        }
+        else {
+            alert('Must contain letters only.')
+        }
+});
