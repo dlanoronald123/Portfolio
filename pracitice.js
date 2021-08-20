4ec1c7d6b78832778047c2e8e847f1f2c74c2170
Ronald Magno <dlanoronald123@gmail.com>
2021-08-20 20:01:21 +0800

    Added js file for practice

---
+++ pracitice.js
@@ -0,0 +1,34 @@
+//Reverse a String 
+
+// const rev = (string) => {
+//   const arr = string.split("");
+//   arr.reverse();
+//   string = arr.join("");
+//   console.log(string);
+// }
+
+// (function (string){
+//   let reversed = "";
+//   for(let character of string){
+//     reversed = character + reversed;
+//   }
+//   console.log({
+//     reversed_value:reversed
+//   });
+// })("Sample!");
+
+//Palindrome 
+// Reverse the string
+// Check orig string if they are the same
+// const Palindrome = (str) => {
+//   const isPalindrome = str.split("").reverse().join("");
+//   console.log(str === isPalindrome);
+// }
+
+// (function (str){
+//   let reversed = '';
+//   for (let x of str){
+//     reversed = x + reversed;
+//   }
+//   console.log(str===reversed)
+// })("Sample")
