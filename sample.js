4403f9199c5b2e12f90f5ad96564aa766c636fac
Ronald Magno <dlanoronald123@gmail.com>
2021-09-16 19:37:12 +0800

    Update samples

---
+++ sample.js
@@ -0,0 +1,9 @@
+// function Compute() {
+//   let celsius = document.getElementById('celsius').value;
+//   let result = (celsius * 1.8)+ 32;
+//   var fahrenheit = document.getElementById('fahrenheit').innerHTML = `${celsius} celsius = ${result} fahrenheit`;
+// }
+
+// function Reset(){
+//   location.reload();
+// }
--- test.js
+++ test.js
@@ -103,7 +103,7 @@
 //   // Sort companies by start year
   
 //   // const sortedCompanies  = companies.sort(function(c1, c2) {
-//   //   if(c1.start > c2.start) {
+//   //   if(c1.start > c2.start) { 
 //   //     return 1;
 //   //   } else {
 //   //     return -1;
