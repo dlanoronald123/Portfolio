ae2fb4e4e176c332e98d03979962fc6e370e7fe9
Ronald Magno <dlanoronald123@gmail.com>
2021-09-24 23:20:44 +0800

    Added Task Tracker on portfolio

--- median.js
+++ median.js
@@ -1,36 +1,62 @@
 
 
-const str = '2 5 7 9 10 1 4 8 3'
+// const str = '2 5 7 9 10 1 4 8 3'
 
-const compose = (a,b,c,d) => (data) => a(b(c(d(data))))
+// const compose = (a,b,c,d) => (data) => a(b(c(d(data))))
 
-const makeArr = (str) => {
-    return str.split(' ')
-}
+// const makeArr = (str) => {
+//     return str.split(' ')
+// }
 
-const cleanData = (arr) => {
-    return arr.map(elem => parseInt(elem))
-}
+// const cleanData = (arr) => {
+//     return arr.map(elem => parseInt(elem))
+// }
 
-//Bubble Sort
-const sort = (arr) =>{
-    let temp 
-    for (let i = arr.length; i > 0; i--){
-        for (let j = 0; j < i; j++){
-            if(arr[j] > arr[j + 1]){
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
-            }
+// //Bubble Sort
+// const sort = (arr) =>{
+//     let temp 
+//     for (let i = arr.length; i > 0; i--){
+//         for (let j = 0; j < i; j++){
+//             if(arr[j] > arr[j + 1]){
+//                 temp = arr[j]
+//                 arr[j] = arr[j + 1]
+//                 arr[j + 1] = temp
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+// const getMedian = (arr) => {
+//     const isEven = arr.length % 2 === 0
+//     return isEven ? arr[arr.length / 2 - 1] : arr[Math.floor(arr.length / 2)]
+// }
+
+// const composedGetNum = compose(getMedian, sort, cleanData, makeArr)
+// console.log(composedGetNum(str))
+
+// var arr = [];
+// for(var i=0; i<10; i++){
+//     var f = function(){
+//         console.log(i);
+//     }
+//     arr.push(f);
+// }
+// for(var j=1; j<arr.length; j++){
+//     arr[j]();
+// }
+
+var arr = [];
+for(var i=0; i<10; i++){
+    var f = function(i){
+        return function(){
+            console.log(i);
         }
-    }
-    return arr
+    }(i);
+    arr.push(f);
 }
-
-const getMedian = (arr) => {
-    const isEven = arr.length % 2 === 0
-    return isEven ? arr[arr.length / 2 - 1] : arr[Math.floor(arr.length / 2)]
+for(var j=1; j<arr.length; j++){
+   if(arr[j] != arr[j-1]){
+       arr[j]();
+   }
 }
-
-const composedGetNum = compose(getMedian, sort, cleanData, makeArr)
-console.log(composedGetNum(str))
